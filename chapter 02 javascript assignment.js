69ff50a851ccb56fd0f9094c0423c91263d9d4e3
GitWizard <nimraghouri97@gmail.com>
2025-01-13 01:53:28 +0500

    Update chapter 02 javascript assignment.js

--- chapter 02 javascript assignment.js
+++ chapter 02 javascript assignment.js
@@ -7,7 +7,41 @@ console.log(myName);
 let message;
 message ="HELLO WORLD"
 alert(message);
-// QUESTION NO 
+// QUESTION NO 4
+let studentName = "nimra ghouri"
+let studentClass = "web and application development"
+let studentSubject = "programing"
+let studentCity = "karachi"
+
+alert("student Name:"  +  studentName);
+alert("student Class:"  +  studentClass);
+alert("tudent Subject:"  +  studentSubject);
+alert("student City:"  +  studentCity);
+
+// QUESTION NO 5
+let a = "pizza"
+let b = "pizz"
+let c = "piz"
+let d = "pi"
+let e = "p"
+
+alert(a)
+alert(b)
+alert(c)
+alert(d)
+alert(e)
+
+// QUESTION NO 6
+let email = "example@example.com";
+alert(email);
+
+// QUESTION NO 7
+let book = "A smarter way to learn JavaScript";
+alert("I am reading the book: " + book);
+
+// QUESTION NO 8
+let abc = "yah! i can HTML contant through javascript"
+console.log (abc);
 
 
 
