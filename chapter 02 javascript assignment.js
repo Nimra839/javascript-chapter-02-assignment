ae195958aaab1c937feab70268f3cf5f38220ae3
GitWizard <nimraghouri97@gmail.com>
2025-01-12 23:53:00 +0500

    Add files via upload

---
+++ chapter 02 javascript assignment.js
@@ -0,0 +1,13 @@
+// QUESTION NO 1
+let userName;
+// QUESTION NO 2
+myName = "nimra ghouri";
+console.log(myName);
+// QUESTION NO 3
+let message;
+message ="HELLO WORLD"
+alert(message);
+// QUESTION NO 
+
+
+
